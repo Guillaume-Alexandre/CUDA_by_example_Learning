79bc45dbf94a9c1c0d4337b1017d1a5891b16421
Guillaume-Alexandre Chaizy-Gostovitch <guillaume.chaizy@etu.emse.fr>
2017-03-14 14:41:39 +0100

    C10.P4

--- CUDA_by_example_Learning/kernel.cuh
+++ CUDA_by_example_Learning/kernel.cuh
@@ -3,29 +3,28 @@
 #ifndef DEFINITIONS
 #define DEFINITIONS
 
-#define DIM 800
-#define PI 3.14
+#define N (1024*1024)
+#define FULL_DATA_SIZE (N*20)
 
 #endif // !DEFINITIONS
 
 #ifndef STRUCT
 #define STRUCT
 
-struct structure {
-	int int_structure = 0;
-};
-
 #endif // !STRUCT
 
 #ifndef HEAD_KERNEL
 #define HEAD_KERNEL
 
-
-__global__ void kernel(void);
-
-__global__ void kernel(void) {
-	printf("Hello World!");
-}
+__global__ void kernel(int *a, int *b, int *c) {
+	int idx = threadIdx.x + blockIdx.x * blockDim.x;
+	if (idx < N) {
+		int idx1 = (idx + 1) % 256;
+		int idx2 = (idx + 2) % 256;
+		float as = (a[idx] + a[idx1] + a[idx2]) / 3.0f;
+		float bs = (b[idx] + b[idx1] + b[idx2]) / 3.0f;
+		c[idx] = (as + bs) / 2;
+	}
 
 
 
